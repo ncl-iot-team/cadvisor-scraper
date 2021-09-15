5afc6a63daaaa5ce5290dd5a2f47320443297df1
Ringo Sham <ringosham@gmail.com>
2021-09-15 15:48:20 +0100

    Initial commit

---
+++ src/main/java/com/ringosham/metric/CPU.java
@@ -0,0 +1,26 @@
+package com.ringosham.metric;
+
+public class CPU {
+    private final long user;
+    private final long system;
+    private final long total;
+
+    public CPU(long user, long system, long total) {
+        this.user = user;
+        this.system = system;
+        this.total = total;
+    }
+
+    public long getUser() {
+        return user;
+    }
+
+    public long getSystem() {
+        return system;
+    }
+
+    public long getTotal() {
+        return total;
+    }
+
+}
